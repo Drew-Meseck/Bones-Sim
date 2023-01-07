50c10e7e470c8de64ccb5341e97387827ec39f67
Drew-Meseck <dm3306a@student.american.edu>
2023-01-07 13:39:19 -0700

    Finished scoring dict, test player creation

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import random
 import pandas
+from Player import Player
 
 RNG = random.SystemRandom()
 SCORE_LIMIT = 10000
-NUM_PLAYERS = 4
+NUM_PLAYERS = 5
+
+def setup():
+    players = []
+    for i in range(NUM_PLAYERS):
+        players.append(Player(i))
+    return players
+
 
 def turn(players):
     for player in players:
         #Here is where the game logic for a player taking their turn is outlined
-        pass
+        print(player.id)
 
 
 
 if __name__ == "__main__":
-    pass
+    players = setup()
+    turn(players)
 
