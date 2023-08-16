b39fb8b12fb654137aaa658216bb0fbe272cf14a
Drew-Meseck <dm3306a@student.american.edu>
2023-08-15 22:03:16 -0600

    Dice Draw Test

---
+++ src/dice.py
@@ -0,0 +1,13 @@
+import pygame
+
+#create dice class
+class Dice(pygame.sprite.Sprite):
+    def __init__(self, col, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(col)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+    
+    def roll(self):
+        pass
--- src/main.py
+++ src/main.py
@@ -1,20 +1,32 @@
 import pygame
 import sys
+import random as rand
+from dice import Dice
 
 pygame.init()
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((1280,720))
 pygame.display.set_caption('my game')
-
 run = True
 
+dice = pygame.sprite.Group()
+dice.add(Dice('white', 500, 300))
+
+
+
 while run:
-    screen.fill((0,0,0))
+    screen.fill('dark green')
+    dice.update()
+    dice.draw(screen)
+    print(dice)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    
     
     pygame.display.update()
     clock.tick(60)
